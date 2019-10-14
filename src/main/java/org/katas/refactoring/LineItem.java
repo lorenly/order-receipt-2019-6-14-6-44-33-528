9983885a5fd393e090098fc3c9043123df2ef401
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-14 17:16:47 +0800

    Changed variable p to price

--- src/main/java/org/katas/refactoring/LineItem.java
+++ src/main/java/org/katas/refactoring/LineItem.java
@@ -2,13 +2,13 @@ package org.katas.refactoring;
 
 public class LineItem {
     private String desc;
-    private double p;
+    private double price;
     private int qty;
 
     public LineItem(String desc, double p, int qty) {
         super();
         this.desc = desc;
-        this.p = p;
+        this.price = p;
         this.qty = qty;
     }
 
@@ -17,7 +17,7 @@ public class LineItem {
     }
 
     public double getPrice() {
-        return p;
+        return price;
     }
 
     public int getQuantity() {
@@ -25,6 +25,6 @@ public class LineItem {
     }
 
     double totalAmount() {
-        return p * qty;
+        return price * qty;
     }
 }
