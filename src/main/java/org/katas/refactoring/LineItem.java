b6966018c233a9f610eb31b22268f26a5b3ec89b
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-14 17:29:03 +0800

    Update variable names

--- src/main/java/org/katas/refactoring/LineItem.java
+++ src/main/java/org/katas/refactoring/LineItem.java
@@ -1,19 +1,18 @@
 package org.katas.refactoring;
 
 public class LineItem {
-    private String desc;
+    private String description;
     private double price;
     private int qty;
 
     public LineItem(String desc, double price, int qty) {
-        super();
-        this.desc = desc;
+        this.description = desc;
         this.price = price;
         this.qty = qty;
     }
 
     public String getDescription() {
-        return desc;
+        return description;
     }
 
     public double getPrice() {
